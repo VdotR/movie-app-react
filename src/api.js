19ced6db49951fedd7745080eff7cc55269b6772
Victor Ren <victorren2002@gmail.com>
2025-05-12 23:04:25 -0700

    removed unnecessary try block

--- src/api.js
+++ src/api.js
@@ -24,71 +24,67 @@ export const fetchMovieDetails = async (id) => {
 
 
 export const login = async ({ username, password }) => {
-    try {
-        // Step 1: Get request token
-        const options1 = {method: 'GET', headers};
-        const res1 = await fetch('https://api.themoviedb.org/3/authentication/token/new', options1)
-        if (!res1.ok) throw new Error('Step 1: Failed to fetch request token');
-        const res1JSON = await res1.json();
-        //console.log(res1JSON);
-        const requestToken = res1JSON.request_token;
-        //console.log(requestToken);
+    // Step 1: Get request token
+    const options1 = {method: 'GET', headers};
+    const res1 = await fetch('https://api.themoviedb.org/3/authentication/token/new', options1)
+    if (!res1.ok) throw new Error('Step 1: Failed to fetch request token');
+    const res1JSON = await res1.json();
+    //console.log(res1JSON);
+    const requestToken = res1JSON.request_token;
+    //console.log(requestToken);
 
-        //Step 2: Login with username and password and Validate request token
-        const options2 = {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({
-                username,
-                password,
-                request_token: requestToken
-            })
-        };
-
-        const res2 = await fetch('https://api.themoviedb.org/3/authentication/token/validate_with_login', options2)
-        //console.log(res2);
-        if (!res2.ok) throw new Error('Step 2: Failed to validate request token');
-        //const validateTokenContent = await res2.json();
-        //console.log(validateTokenContent);
-        // Step 3: Create session ID
-        const options3 = {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({request_token: requestToken})
-        };
-
-        const res3 = await fetch('https://api.themoviedb.org/3/authentication/session/new', options3)
-        if (!res3.ok) throw new Error('Step 3: Failed to create session ID');
-        const sessionContent = await res3.json();
-        //console.log(sessionContent);
-        const sessionId = sessionContent.session_id;
-        //console.log("Session id: " + sessionId);
-        
-        // Step 4: get account ID
-        const options4 = {
-            method: 'GET',
-            headers
-        };
-
-        const res4 = await fetch(`https://api.themoviedb.org/3/account?api_key=}&session_id=${sessionId}`, options4)
-        if (!res4.ok) throw new Error('Step 4: Failed to get account ID');
-        const accountContent = await res4.json();
-        const accountId = accountContent.id;
-        
-        // Step 5: Store everything in local storage
-        const userData = {
+    //Step 2: Login with username and password and Validate request token
+    const options2 = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
             username,
-            accountId,
-            sessionId,
-            requestToken
-        }
+            password,
+            request_token: requestToken
+        })
+    };
 
-        localStorage.setItem('userData', JSON.stringify(userData));
+    const res2 = await fetch('https://api.themoviedb.org/3/authentication/token/validate_with_login', options2)
+    //console.log(res2);
+    if (!res2.ok) throw new Error('Step 2: Failed to validate request token');
+    //const validateTokenContent = await res2.json();
+    //console.log(validateTokenContent);
+    // Step 3: Create session ID
+    const options3 = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({request_token: requestToken})
+    };
 
-        return userData;
-    } catch (e) {
-        throw e;
+    const res3 = await fetch('https://api.themoviedb.org/3/authentication/session/new', options3)
+    if (!res3.ok) throw new Error('Step 3: Failed to create session ID');
+    const sessionContent = await res3.json();
+    //console.log(sessionContent);
+    const sessionId = sessionContent.session_id;
+    //console.log("Session id: " + sessionId);
+    
+    // Step 4: get account ID
+    const options4 = {
+        method: 'GET',
+        headers
+    };
+
+    const res4 = await fetch(`https://api.themoviedb.org/3/account?api_key=}&session_id=${sessionId}`, options4)
+    if (!res4.ok) throw new Error('Step 4: Failed to get account ID');
+    const accountContent = await res4.json();
+    const accountId = accountContent.id;
+    
+    // Step 5: Store everything in local storage
+    const userData = {
+        username,
+        accountId,
+        sessionId,
+        requestToken
     }
+
+    localStorage.setItem('userData', JSON.stringify(userData));
+
+    return userData;
 };
 
 export const getLikedMovies = async ({ sessionId, accountId, likedPage }) => {
